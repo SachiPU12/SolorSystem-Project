2371d39f6e3f27b4b5a7961db703def51f3a8ec9
Sachi Udawaththa <83857134+SachiPU12@users.noreply.github.com>
2021-07-19 20:32:08 +0530

    Add files via upload

---
+++ src/solor/system/SolorSystem.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solor.system;
+
+/**
+ *
+ * @author Internet
+ */
+public class SolorSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new JFrame_Main().setVisible(true);
+    }
+    
+}
